8245bb9e2dd451160b89284d2a1acd37c5aa73fc
Krzysztof Kozioł <me@krko.eu>
2022-10-04 17:27:34 +0200

    20221004-1

---
+++ dist/src_bootstrap_js.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkmy_action"] = self["webpackChunkmy_action"] || []).push([["src_bootstrap_js"],{
+
+/***/ "./src/bootstrap.js":
+/*!**************************!*\
+  !*** ./src/bootstrap.js ***!
+  \**************************/
+/***/ (function() {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (16:2)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| \\n| ReactDOM.render(\\n>   <App />,\\n|   document.getElementById(\\n|     'root'\");\n\n//# sourceURL=webpack://my-action/./src/bootstrap.js?");
+
+/***/ })
+
+}]);
